1f1092d26921cbe48880d4a910658d4ee652f2b7
okee-sheet <75809272+okee-sheet@users.noreply.github.com>
2022-01-31 01:14:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     titleWeb: "Yêu cậu 💝",
     introTitle: 'ey người yêu tương lai của tớ (づ￣ 3￣)づ',
-    introDesc: `Nghe đồn anh thích màu xanh?
-Còn em thì lại thích anh chẳng rời.
-Hình như anh thích bầu trời?
-Còn em thì thích một đời bên anh.`,
+    introDesc: `Nghe đồn anh thích màu xanh?'/n
+'Còn em thì lại thích anh chẳng rời.'/n
+'Hình như anh thích bầu trời?'/n
+'Còn em thì thích một đời bên anh.`/n ,
     btnIntro: '^^From Thùy with love^^',
     title: 'Phải chăng cậu đã yêu Thùy ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng cậu đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Dell 🙂',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    btnReply: 'Nói lời yêu thương cho Thùy đi <3',
     reply: 'Vì lỡ yêu Thùy quá rồi biết phải làm sao <33333333',
     mess: 'Mình biết mà 🥰. Thùy cũng yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình đi chơi nha ヾ(≧▽≦*)o.',

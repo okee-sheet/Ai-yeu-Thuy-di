ef5674d3a6077511f83b966fbdf61ca265442937
okee-sheet <75809272+okee-sheet@users.noreply.github.com>
2022-01-31 01:00:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Yêu cậu 💝",
-    introTitle: 'hey người yêu tương lai của tớ',
+    introTitle: 'hey người yêu tương lai của tớ (づ￣ 3￣)づ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Tối nay 7h, mình đi chơi nha ヾ(≧▽≦*)o.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100009425164177' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
